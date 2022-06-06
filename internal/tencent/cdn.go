e062fcc3306695889ed9819b6c559278d2cb6f23
guyskk <guyskk@qq.com>
2022-06-06 14:51:28 +0800

    update cdn cache time

--- internal/tencent/cdn.go
+++ internal/tencent/cdn.go
@@ -103,7 +103,7 @@ func getNodeCacheRules() []*cdn.RuleCache {
 }
 
 func getBrowserCacheRules() []*cdn.MaxAgeRule {
-	indexMaxAgeTime := int64Ref(30 * 60)
+	indexMaxAgeTime := int64Ref(3 * 60)
 	staticMaxAgeTime := int64Ref(10 * 24 * 60 * 60)
 	return []*cdn.MaxAgeRule{
 		{
